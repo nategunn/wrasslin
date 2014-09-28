cb294731b2bcdef3b2c3156735119f5148c9e154
Nate Gunn <nategunn2000@gmail.com>
2014-09-27 21:23:24 -0700

    new handleclick, assign, and attack functions
    using radio button to select wrestler
    handleclick function directs selection to the
    assign function which assigns wrestler attributes to different variables
    attack function generates a random attack(from the moves array) along
    with its hit point damage. It also generates commentary based on the
    selected attack

--- wrestlingGame.js
+++ wrestlingGame.js
@@ -1,41 +1,74 @@
-
-
 var ToolBelt = {
 
-	
-	
-	life:50,
-	punDam:[3,2,4,5],
+commentary:{
 	exclamations: ["Wow! ", "Oh no! ", "God almighty! ", "Good Gracious! "],
-	leadIn: ["That was a nasty ", "What a powerful ", "A horrifying ", "That was an illegal "], 
-	strikes: ["uppercut!", "chairshot!", "Lucy lick!", "monkey punch!", "jab", "Bug meow!", "dropkick!"],
-	done: ["You should stop.", "Is he still breathing?", "Someone call the cops!", "ok, turn the lights out when you leave."],
+	leadIn: ["That was a nasty ", "What a powerful ", "A horrifying ", "That was an illegal "]
+},
 
-	punch:function(){
+moves:[
+	["dropkick", 5],
+	["clothesline", 4],
+	["backdrop", 4],
+	["punch", 3]
+],
 
-		var x = ToolBelt.random(0,ToolBelt.punDam.length);
-		var exc = ToolBelt.random(0,ToolBelt.exclamations.length);
-		var lead = ToolBelt.random(0,ToolBelt.leadIn.length);
-		var str = ToolBelt.random(0,ToolBelt.strikes.length);
-		var dn = ToolBelt.random(0,ToolBelt.done.length);
-		var string = ToolBelt.exclamations[exc] + ToolBelt.leadIn[lead] + ToolBelt.strikes[str];
+wrestler:[
+{
+	hitPoints: 50,
+	moveset: 1,
+	name: "Zeb Jackson",
+	nickname: "Hulk",
+	finisher: "the Hulk Smash"
+},
+{
+	hitPoints: 50,
+	moveset: 1,
+	name: "Jack Hogan",
+	nickname: "Lightning",
+	finisher: "Electrocutioner"
+}
+],
 
-		ToolBelt.life -= ToolBelt.punDam[x];
 
-		document.getElementById("lif").innerHTML = ToolBelt.life + " hit points left!";
 
-		if(ToolBelt.life <= 0){
-			document.getElementById("commentator").innerHTML = ToolBelt.done[dn];
-		}else{
-			document.getElementById("commentator").innerHTML = string;
-		};
-	},
+currentWrasslerKey: null,
+hp1: null,
 
-	random: function(max, min){
+
+handleClick: function(el){
+	this.currentWrasslerKey = el.value;
+},
+
+assign: function(){
+	if(this.currentWrasslerKey==null){return;}
+	var x = this.currentWrasslerKey;
+	var i = this.wrestler[x].name;
+	this.hp1 = this.wrestler[x].hitPoints;
+	
+},
+
+attack: function(){
+	if(this.currentWrasslerKey==null){return;}
+	var string ='';
+	for(key in this.commentary){
+		var comm = this.commentary[key];
+		var x = this.random(0,comm.length);
+		string += comm[x] + " ";
+	} 
+
+	var rand = this.random(0, this.moves.length);
+	var moveName = this.moves[rand][0];
+	string += moveName;
+
+	var dam = this.moves[rand][1];
+	this.hp1 -= dam;
+
+	document.getElementById("commentator").innerHTML = string;
+	document.getElementById("lif").innerHTML = this.hp1;
+},
+
+random: function(max, min){
 		return Math.floor(Math.random()*(max-min)+min);
-	}
-  
+}
 
 };
-
-
